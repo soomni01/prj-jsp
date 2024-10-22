5019bdd8d527a075e5d607fa2652eb6fcd5dc371
soomni01 <dsgh8933@naver.com>
2024-10-22 10:29:52 +0900

    게시물 저장 후 redirect

--- src/main/java/com/example/prjjsp20241022/controller/BoardController.java
+++ src/main/java/com/example/prjjsp20241022/controller/BoardController.java
@@ -1,14 +1,19 @@
 package com.example.prjjsp20241022.controller;
 
+import com.example.prjjsp20241022.dto.Board;
+import com.example.prjjsp20241022.service.BoardService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 @RequiredArgsConstructor
 @RequestMapping("/board")
 public class BoardController {
+
+    private final BoardService service;
     // 게시물 CRUD
 
     // /board/new
@@ -16,5 +21,12 @@ public class BoardController {
     public void newBoard() {
 
         // /WEB-INF/view/board/new.jsp
+    }
+
+    @PostMapping("new")
+    public String newBoard(Board board) {
+        service.add(board);
+
+        return "redirect:/board/new";
     }
 }
--- src/main/webapp/WEB-INF/view/board/new.jsp
+++ src/main/webapp/WEB-INF/view/board/new.jsp
@@ -6,18 +6,19 @@
 </head>
 <body>
 <h2>새 게시물 작성</h2>
-<form method="post"></form>
-<div>제목
-    <input type="text" name="title">
-</div>
-<div>본문
-    <textarea name="content" id="" cols="30" rows="10"></textarea>
-</div>
-<div>작성자
-    <input type="text" name="writer">
-</div>
-<div>
-    <button>저장</button>
-</div>
+<form method="post">
+    <div>제목
+        <input type="text" name="title">
+    </div>
+    <div>본문
+        <textarea name="content" id="" cols="30" rows="10"></textarea>
+    </div>
+    <div>작성자
+        <input type="text" name="writer">
+    </div>
+    <div>
+        <button>저장</button>
+    </div>
+</form>
 </body>
 </html>
