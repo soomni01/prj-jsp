20d77c18d772835acd4e5f1dd8e8670f3635e9be
soomni01 <dsgh8933@naver.com>
2024-10-28 11:27:35 +0900

    한 게시물 조회 시 작성자 별명 출력

--- sql/member.sql
+++ sql/member.sql
@@ -24,3 +24,13 @@ INSERT INTO auth
     (id, name)
 VALUES ('admin', 'admin'),
        ('bdmin', 'admin');
+
+SELECT *
+FROM board;
+SELECT *
+FROM member;
+
+# 게시물의 writer 값을 member에 있는 값으로 update
+UPDATE board
+SET writer = (SELECT id FROM member LIMIT 1)
+WHERE id > 0;
